c6fe9d6bf1fbf7f0e6bef9fba7c80d7f44432c09
rahmivinnn <rahmivina230@gmail.com>
2025-07-10 14:32:20 +0700

    Remove all music-beat-detector references, fix blank page, browser-only BPM detection

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,11 +1,1 @@
 /// <reference types="vite/client" />
-
-declare module 'music-beat-detector' {
-  interface BeatDetectionResult {
-    tempo: number;
-    beats: number[];
-  }
-  
-  function detectBeats(audioBlob: Blob): Promise<BeatDetectionResult>;
-  export = detectBeats;
-}
